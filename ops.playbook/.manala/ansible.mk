e501b0aea4ce5c84de3bae276a205cc99a00f712
loic-roux <69loloro10@gmail.com>
2020-05-16 16:55:14 +0200

    ansible extra vars

--- ops.playbook/.manala/ansible.mk
+++ ops.playbook/.manala/ansible.mk
@@ -1,24 +1,22 @@
 # Base makefile for ansible / vagrant projects
 SHELL = /bin/bash
 # read setting from config
-CONFIG=config.json
-config = $(shell yq r config.yaml $(1) || false)
+config = $(shell yq r .manala.yaml $(1))
 # All variables necessary to run and debug ansible playbooks
 PLAYBOOKS=$(basename $(wildcard *.yml))
-IP?=$(call config,network.ip)
-DOMAIN?=$(call config,domain)
+IP?=$(call config,vagrant.network.ip)
+DOMAIN?=$(call config,vagrant.domain)
 # ansible vars
-keyboard_mac?=$(call config,ansible.vars.keyboard_mac)
-# ansible-playbook arguments
-OPTIONS:= -e keyboard_mac=$(keyboard_mac)
+BASHED_YAML:=$(shell echo -n $(call config,vagrant.ansible.vars) | sed -e 's/:[^:\/\/]/="/g;s/$$/"/g;s/ *=/=/g')
+OPTIONS:= $(foreach var, $(BASHED_YAML), -e $(subst ",,$(var)))
 # Environment variables of ansible 
 ANSIBLE_STDOUT_CALLBACK:=default
 # Default Inventory
-INVENTORY?= $(call config,ansible.inventory)
+INVENTORY?= $(call config,ansible.default_inventory)
 playbook_exe= \
 	ANSIBLE_STDOUT_CALLBACK=$(ANSIBLE_STDOUT_CALLBACK) \
 	ansible-playbook $(OPTIONS) \
-	-i ./inventories/$(INVENTORY) $*.yml $(ARG)
+	-i $(INVENTORY) $*.yml $(ARG)
 # Prompt exe
 prompt?=echo -ne "\x1b[33m $(1) Sure ? [y/N]\x1b[m" && read ans && [ $${ans:-N} = y ]
 
@@ -28,7 +26,7 @@ help:
 	@echo "Available PLAYBOOKS: $(PLAYBOOKS)"
 	@$(MAKE) help_more || true
 	@echo "[========== OPTIONS ===========]"
-	@echo "keyboard_mac: $(keyboard_mac)"
+	@echo "$(BASHED_YAML)"
 	@echo "Ip: $(IP)"
 	@echo "Domain: $(DOMAIN)"
 	@echo "default inventory: $(INVENTORY)"
@@ -42,6 +40,7 @@ $(PLAYBOOKS): % : %.run
 install:
 	ansible-galaxy install -r roles/requirements.yml $(ARG)
 	$(foreach var,$(shell ls -d *roles/role*/requirements.txt),pip install -r $(var);)
+	$(foreach var,$(shell ls -d *.ext_roles/role*/requirements.txt),pip install -r $(var);)
 
 # ==============================
 # Warning run target is for prod
@@ -56,3 +55,18 @@ install:
 
 running:
 	@echo "RUNNING $(DEBUG): \n"${playbook_exe}
+
+# =============================
+# Debugging zone on next lines
+# =============================
+OPTIONS+=\
+	-e ansible_host=$(DOMAIN)\
+	-e ansible_port=22\
+	-e ansible_user=vagrant\
+
+debug: 
+	$(eval ANSIBLE_STDOUT_CALLBACK:=yaml)
+	@$(MAKE) running DEBUG='IN DEBUG MODE '
+
+test-yaml:
+	@echo $(OPTIONS)
